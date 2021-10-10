3d1809327061da968578de98ddf72481e8b9116c
Moão <moaojarco@gmail.com>
2021-10-10 19:57:14 -0400

    fix tabs colors

--- demo/react.jsx
+++ demo/react.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "redux";
+import React, { Component } from 'react';
+import { connect } from 'redux';
 
 const Header = () => {
   <header className="container" />;
